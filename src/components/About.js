5f5cfa060ed7556df456fe97775c0978d2f8d8ef
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-03-23 11:56:46 -0600

    started about me

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,49 @@ import './styles/About.css'
 
 function About() {
 
-    return<> 
-    
-        <p> about me works!</p>
-    
+    return <>
+
+        <div className="jump-wrapper">
+
+            <a href="#jump-about"> 1 </a>
+            <a href="#jump-edu">  2</a>
+            <a href="#jump-exp"> 3 </a>
+            <a href="#jump-med"> 4 </a>
+
+        </div>
+
+        <div className="container">
+
+            <div className="about-me">
+                <h2 id="jump-about"> About Me </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+            <div className="education">
+                `` <h2 id="jump-edu"> Education </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+
+            <div className="experience">
+                <h2 id="jump-exp"> Experience </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+            <div className="media">
+                <h2 id="jump-med"> Media </h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+        </div>
     </>
 }
 export default About
