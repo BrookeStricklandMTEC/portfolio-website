ff6d69f0cbb9456e19dcb53263849e9f8fa1b067
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-03-25 10:43:49 -0600

    about page started, fixing bug

--- src/components/About.js
+++ src/components/About.js
@@ -6,47 +6,90 @@ function About() {
 
     return <>
 
-        <div className="jump-wrapper">
+        <div className="wrapper">
 
-            <a href="#jump-about"> 1 </a>
-            <a href="#jump-edu">  2</a>
-            <a href="#jump-exp"> 3 </a>
-            <a href="#jump-med"> 4 </a>
+            <div className="jump-wrapper">
+
+                <a href="#jump-about"> 1 </a>
+                <a href="#jump-edu">  2</a>
+                <a href="#jump-exp"> 3 </a>
+                <a href="#jump-med"> 4 </a>
+
+            </div>
+
+            <div className="container">
+                {/* flex center wont work, get help to try to center heap-p-flex */}
+
+                <div className="hea-p-flex">
+
+                    <h2 id="jump-about" className="head-wrap"> About Me </h2>
+                    <div className="about-d">
+                        <h3> Brooke strickland </h3>
+                        <h6> Web Developer </h6>
+
+                        <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+
+                        <div className="skill-wrapper">
+
+                            <div className="skill-cont">
+                                <div className="skills javas"> 65% </div>
+                            </div>
+
+                            <div className="skill-cont">
+                                <div className="skills s-css"> 80% </div>
+                            </div>
+
+                            <div className="skill-cont">
+                                <div className="skills node"> 50% </div>
+                            </div>
+
+                            <div className="skill-cont">
+                                <div className="skills jquery"> 70% </div>
+
+                            </div>
+
+                            <div className="skill-cont">
+                                <div className="skills react"> 80% </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div className="hea-p-flex-end">
+
+                    <h2 id="jump-about" className="head-wrap"> Education </h2>
+
+                    <div className="about-d-2">
+                        <i> </i>
+                        <h3> Mountainland Technical College </h3>
+                        <h6> Web Developement </h6>
+
+                        <p>  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                            sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+
+                </div>
+
+
+
+            </div>
 
         </div>
 
-        <div className="container">
-
-            <div className="about-me">
-                <h2 id="jump-about"> About Me </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-
-            <div className="education">
-                `` <h2 id="jump-edu"> Education </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-
-
-            <div className="experience">
-                <h2 id="jump-exp"> Experience </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-
-            <div className="media">
-                <h2 id="jump-med"> Media </h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-
-        </div>
+        <footer>
+            <Link to={'/'}> home </Link>
+        </footer>
     </>
 }
 export default About
