8d50c87efef84845bc690c2a9e996944dff86840
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-04-19 12:00:29 -0600

    styling issues

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,17 +2,20 @@
 import "./card.css";
 import FlippableCard2 from "./flippable-card-2";
 
-function Card({onClick, flipped}){
+function Card({ onClick, flipped }) {
 
-  const cardImages = ['registration.png', 'golfapp.png']
+  const cardImages = ['registration.png', 'golfapp.png',]
 
-  return cardImages.map( index => {
-    return (<FlippableCard2>
-      <div className="card-back"> Back </div>
-      <div className="card-front">
-       <img src={'./images/'+ index} width={'300px'} height={'500px'}/>
-      </div>
-    </FlippableCard2>)
+  return cardImages.map(index => {
+    return (
+
+      <FlippableCard2>
+          <div className="card-back"> Back </div>
+          <div className="card-front">
+            <img src={'./images/' + index} width={'300px'} height={'500px'} />
+          </div>
+      </FlippableCard2>
+    )
   });
 }
 
