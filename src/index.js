85748dd27b7c80be4c9e292fe76cfd8d6a2b153a
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-04-26 09:06:22 -0600

    gh-pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
-
+import {Router } from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router> 
+         <App />
+    </Router>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
